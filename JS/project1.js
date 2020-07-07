081a1809faddd30cd04e8be2fd933e35c31318cd
Phoenix <phoenix.ch13@gmail.com>
2020-07-07 13:20:37 +0800

    Modified screen resize code.

--- JS/project1.js
+++ JS/project1.js
@@ -1,13 +1,37 @@
-window.onload = ResizeGameWindow;
-window.onresize = ResizeGameWindow;
+// Automatically resizes the canvas to fit browser size.
 
 function ResizeGameWindow() {
-	var GameWindow = document.getElementById('GameWindow');
-
-	GameWindow.setAttribute('width', window.innerWidth);
-	GameWindow.setAttribute('height', window.innerHeight);
+	var GameWindow = {
+		object: document.getElementById('GameWindow'),	
+		w:1280,
+		h: 800		
+	}
 	
-	var context = GameWindow.getContext("2d");
-	context.fillStyle = "blue";
-	context.fillRect(0, 0, GameWindow.width, GameWindow.height);
+	// New width and height based on the dimensions of the browser.
+	var w, h;
+	if (GameWindow.h / GameWindow.w > window.innerHeight / window.innerWidth) 
+	{
+		h = window.innerHeight;
+		w = h * GameWindow.w / GameWindow.h;
+	} 
+	else {
+		w = window.innerWidth;
+		h = w * GameWindow.h / GameWindow.w;
+	}
+	
+	// Modify CSS based on the calculation.
+	GameWindow.object.style.width = w + "px";
+    GameWindow.object.style.height = h + "px";
+	
+	offsetX = (window.innerWidth - w) / 2;
+    offsetY = (window.innerHeight - h) / 2;
+	
+	GameWindow.object.style.margin = offsetY + "px " + offsetX + "px";
+	
+	var context = GameWindow.object.getContext("2d");
+	context.fillStyle = "black";
+	context.fillRect(0, 0, GameWindow.object.width, GameWindow.object.height);	
 }
+
+window.addEventListener("resize", ResizeGameWindow);
+window.addEventListener("load",ResizeGameWindow);
