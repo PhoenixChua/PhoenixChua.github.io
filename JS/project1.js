449c7436089bd893503f3552deb398639677d13e
Phoenix <phoenix.ch13@gmail.com>
2020-07-08 10:57:01 +0800

    Created game loop.

--- JS/project1.js
+++ JS/project1.js
@@ -1,43 +1,79 @@
-// Automatically resizes the canvas to fit browser size.
+//###Program Namespace###
+var Game = 
+{
+	object: null,	
+	w:1280,
+	h: 800,
+	time: 0,
+	sprites: null
+}
 
-function ResizeGameWindow() {
-	var GameWindow = {
-		object: document.getElementById('GameWindow'),	
-		w:1280,
-		h: 800		
-	}
-	
+// Automatically resizes the canvas to fit browser size.
+function ResizeGameWindow() {	
 	// New width and height based on the dimensions of the browser.
 	var w, h;
-	if (GameWindow.h / GameWindow.w > window.innerHeight / window.innerWidth) 
+	if (Game.h / Game.w > window.innerHeight / window.innerWidth) 
 	{
 		h = window.innerHeight;
-		w = h * GameWindow.w / GameWindow.h;
+		w = h * Game.w / Game.h;
 	} 
 	else {
 		w = window.innerWidth;
-		h = w * GameWindow.h / GameWindow.w;
+		h = w * Game.h / Game.w;
 	}
 	
 	// Modify CSS based on the calculation.
-	GameWindow.object.style.width = w + "px";
-    GameWindow.object.style.height = h + "px";
+	Game.object.style.width = w + "px";
+    Game.object.style.height = h + "px";
 	
 	offsetX = (window.innerWidth - w) / 2;
     offsetY = (window.innerHeight - h) / 2;
 	
-	GameWindow.object.style.margin = offsetY + "px " + offsetX + "px";
+	Game.object.style.margin = offsetY + "px " + offsetX + "px";
 	
-	var context = GameWindow.object.getContext("2d");
-	context.fillStyle = "black";
-	context.fillRect(0, 0, GameWindow.object.width, GameWindow.object.height);
-
-	testdraw = new Image();
-	testdraw.src = "Resources/test.png"; 
-	testdraw.onload = function() {
-		context.drawImage(testdraw,0,0);
-	};
+	var context = Game.object.getContext("2d");
 }
 
-window.addEventListener("resize", ResizeGameWindow);
-window.addEventListener("load",ResizeGameWindow);
+//###Graphics###
+function Graphics_Init() 
+{	
+	Game.sprites = new Image();
+	Game.sprites.src = "Resources/test.png"; 
+}
+
+//###Audio###
+
+//###Main###
+function Draw() {
+	var context = Game.object.getContext("2d");
+	context.clearRect(0, 0, Game.object.width, Game.object.height);
+	context.fillStyle = "black";
+	context.fillRect(0, 0, Game.object.width, Game.object.height);	
+	context.font = "30px Arial";
+	context.fillStyle = "red";
+	context.fillText(Game.time, 10, 50);	
+
+	context.drawImage(Game.sprites,0,0,100,100);
+	
+}
+
+function Loop(newtime) {
+	Update(newtime - Game.time);
+	Draw();
+	Game.time = newtime;
+	window.requestAnimationFrame(Loop)
+}
+
+function Update(dt) {
+}
+
+
+function Init() {
+	Game.object = document.getElementById('GameWindow');
+	Graphics_Init();
+	ResizeGameWindow();
+	window.addEventListener("resize", ResizeGameWindow);
+	window.requestAnimationFrame(Loop);
+}
+
+window.addEventListener("load",Init);
