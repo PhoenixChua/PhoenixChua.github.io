83ae754d64d464e6384565c23d1986e695c693f2
Phoenix <phoenix.ch13@gmail.com>
2020-07-09 14:58:31 +0800

    Sprite testing.

--- JS/graphics.js
+++ JS/graphics.js
@@ -1,31 +1,9 @@
-function Graphics_Init() 
+Game.graphics.init = function() 
 {	
-	Game.sprites = new Image();
-	Game.sprites.src = "Resources/test.png"; 
-}
-
-// Automatically resizes the canvas to fit browser size.
-function ResizeGameWindow() {	
-	// New width and height based on the dimensions of the browser.
-	Game.object.width = Game.w;
-	Game.object.height = Game.h;
-	var w, h;
-	if (Game.h / Game.w > window.innerHeight / window.innerWidth) 
-	{
-		h = window.innerHeight;
-		w = h * Game.w / Game.h;
-	} 
-	else {
-		w = window.innerWidth;
-		h = w * Game.h / Game.w;
-	}
-	
-	// Modify CSS based on the calculation.
-	Game.object.style.width = w + "px";
-    Game.object.style.height = h + "px";
-	
-	offsetX = (window.innerWidth - w) / 2;
-    offsetY = (window.innerHeight - h) / 2;
-	
-	Game.object.style.margin = offsetY + "px " + offsetX + "px";
+	var Sprite = new Image();
+	Sprite.src = "Resources/Images/test2.png"; 
+	Game.graphics.sprites.push(Sprite);
+	Sprite = new Image();
+	Sprite.src = "Resources/Images/test.png"; 
+	Game.graphics.sprites.push(Sprite);
 }
--- JS/project1.js
+++ JS/project1.js
@@ -5,29 +5,39 @@ var Game =
 	w:1200,
 	h: 800,
 	time: 0,
-	sprites: null
+	graphics: 
+	{
+		sprites: []
+	},
+	data: null,
+	audio: null,
+	gameobjects: null
 }
 
 //###Main###
-function Loop(newtime) {
+function Loop(newtime) 
+{
 	Update(newtime - Game.time);
 	Draw();
 	Game.time = newtime;
 	window.requestAnimationFrame(Loop)
 }
 
-function Init() {
-	Game.object = document.getElementById('GameWindow');
-	Graphics_Init();
+function Init() 
+{
+	Game.object = document.getElementById('GameWindow');	
+	Game.graphics.init();
 	ResizeGameWindow();
 	window.addEventListener("resize", ResizeGameWindow);
 	window.requestAnimationFrame(Loop);
 }
 
-function Update(dt) {
+function Update(dt) 
+{
 }
 
-function Draw() {
+function Draw() 
+{
 	var context = Game.object.getContext("2d");
 	context.clearRect(0, 0, Game.object.width, Game.object.height);
 	context.fillStyle = "black";
@@ -36,15 +46,58 @@ function Draw() {
 	context.fillStyle = "red";
 	context.fillText(Game.time, 10, 50);	
 
-	context.drawImage(Game.sprites,200,0);
-	context.drawImage(Game.sprites,200,100);
-	context.drawImage(Game.sprites,200,200);
-	context.drawImage(Game.sprites,200,300);
-	context.drawImage(Game.sprites,200,400);
-	context.drawImage(Game.sprites,200,500);
-	context.drawImage(Game.sprites,200,600);
-	context.drawImage(Game.sprites,200,700);
-	context.drawImage(Game.sprites,300,700);
+	context.drawImage(Game.graphics.sprites[0],200,0);
+	context.drawImage(Game.graphics.sprites[0],200,100);
+	context.drawImage(Game.graphics.sprites[0],200,200);
+	context.drawImage(Game.graphics.sprites[0],200,300);
+	context.drawImage(Game.graphics.sprites[1],200,400);
+	context.drawImage(Game.graphics.sprites[1],200,500);
+	context.drawImage(Game.graphics.sprites[0],200,600);
+	context.drawImage(Game.graphics.sprites[0],200,700);
+	context.drawImage(Game.graphics.sprites[0],300,0);
+	context.drawImage(Game.graphics.sprites[0],300,100);
+	context.drawImage(Game.graphics.sprites[0],300,200);
+	context.drawImage(Game.graphics.sprites[0],300,300);
+	context.drawImage(Game.graphics.sprites[1],300,400);	
+	context.drawImage(Game.graphics.sprites[1],300,500);		
+	context.drawImage(Game.graphics.sprites[1],300,600);	
+	context.drawImage(Game.graphics.sprites[1],300,700);
+	context.drawImage(Game.graphics.sprites[0],500,0);
+	context.drawImage(Game.graphics.sprites[0],500,100);
+	context.drawImage(Game.graphics.sprites[1],500,200);
+	context.drawImage(Game.graphics.sprites[0],500,300);
+	context.drawImage(Game.graphics.sprites[1],500,400);	
+	context.drawImage(Game.graphics.sprites[1],500,500);		
+	context.drawImage(Game.graphics.sprites[1],500,600);	
+	context.drawImage(Game.graphics.sprites[1],500,700);	
 }
 
-window.addEventListener("load",Init);
+
+// Automatically resizes the canvas to fit browser size.
+function ResizeGameWindow() 
+{	
+	// New width and height based on the dimensions of the browser.
+	Game.object.width = Game.w;
+	Game.object.height = Game.h;
+	var w, h;
+	if (Game.h / Game.w > window.innerHeight / window.innerWidth) 
+	{
+		h = window.innerHeight;
+		w = h * Game.w / Game.h;
+	} 
+	else {
+		w = window.innerWidth;
+		h = w * Game.h / Game.w;
+	}
+	
+	// Modify CSS based on the calculation.
+	Game.object.style.width = w + "px";
+    Game.object.style.height = h + "px";
+	
+	offsetX = (window.innerWidth - w) / 2;
+    offsetY = (window.innerHeight - h) / 2;
+	
+	Game.object.style.margin = offsetY + "px " + offsetX + "px";
+}
+
+window.addEventListener("load", Init);
