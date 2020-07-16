24c52dfb29b1f338aaf9ad376f605164019969cf
Phoenix <phoenix.ch13@gmail.com>
2020-07-16 13:19:19 +0800

    Added stylized links.

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,19 @@ window.onload = function(){
           val = " ";
 
      document.getElementById("testinput").value = val;
+	 
+	// Navigation bar link highlighting.
+	var links = document.getElementsByClassName("link");	 
+	for (var i = 0; i < links.length; i++) 
+	{
+		links[i].addEventListener("click", 
+		function() 
+		{				
+			var current = document.getElementsByClassName("linkactive");
+			current[0].className = current[0].className.replace(" linkactive", "");
+			this.className += " linkactive";
+		});
+	}
 }
 
 window.onbeforeunload = function(){
